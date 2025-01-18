9a686cae2aefbbb0ef04762184cd5e4e85e6d2ee
ShikenNuggets <carter.rennick@gmail.com>
2025-01-18 13:30:45 -0500

    Various improvements - triangle render

--- Source/main.cpp
+++ Source/main.cpp
@@ -27,6 +27,7 @@ int print_buffer(qword_t* b, int len){
 int gs_finish(){
     qword_t buf[50];
     qword_t* q = buf;
+    // TODO - Cleanup
     q = draw_primitive_xyoffset(q, 0, 0, 0);
     q = draw_finish(q);
     dma_channel_send_normal(DMA_CHANNEL_GIF, buf, q - buf, 0, 0);
@@ -35,24 +36,45 @@ int gs_finish(){
 }
 
 int gs_init(int width, int height, int psm, int psmz, int vmode, int gmode){
-    unsigned int fb_address = graph_vram_allocate(width, height, psm, GRAPH_ALIGN_PAGE);
+    framebuffer_t fb{};
+    fb.address = graph_vram_allocate(width, height, psm, GRAPH_ALIGN_PAGE);
+    fb.width = width;
+    fb.height = height;
+    fb.psm = psm;
+    fb.mask = 0;
+
+    zbuffer_t z;
+    z.address = graph_vram_allocate(width, height, psmz, GRAPH_ALIGN_PAGE);
+    z.enable = 0;
+    z.method = 0;
+    z.zsm = psmz;
+    z.mask = 0;
+
     //unsigned int zbuf_address = graph_vram_allocate(w, h, psmz, GRAPH_ALIGN_PAGE);
-    graph_set_mode(gmode, vmode, GRAPH_MODE_INTERLACED, GRAPH_DISABLE);
+    graph_set_mode(gmode, vmode, GRAPH_MODE_INTERLACED, GRAPH_ENABLE);
     graph_set_screen(OFFSET_X, OFFSET_Y, width, height);
     graph_set_bgcolor(0, 0, 0);
-    graph_set_framebuffer_filtered(fb_address, width, psm, 0, 0);
+    graph_set_framebuffer_filtered(fb.address, width, psm, 0, 0);
     graph_enable_output();
-    
+
+    qword_t buf[100];
+    qword_t* q = buf;
+    q = draw_setup_environment(q, 0, &fb, &z);
+    dma_channel_send_normal(DMA_CHANNEL_GIF, buf, q - buf, 0, 0);
+    dma_wait_fast();
+
     gs_finish();
 
     return 0;
 }
 
 static int tri[] = {
-    10, 0, 0,
+    320, 50, 0,
     600, 200, 0,
-    20, 400, 0
+    20, 200, 0
 };
+
+#define SHIFT_AS_I64(x, b) (static_cast<int64_t>(x) << b)
 
 int draw(){
     uint64_t red = 0xf0;
@@ -61,19 +83,27 @@ int draw(){
 
     qword_t buf[50];
     qword_t* q = buf;
-    // GIFTag header - col, pos, col, pos, col, pos
-    q->dw[0] = 0x0000000000515151;
     // 6 registers, x1, EOP
-    q->dw[1] = 0x6000000000000001;
+    q->dw[0] = 0x7000000000000001;
+    // GIFTag header - col, pos, col, pos, col, pos
+    q->dw[1] = 0x0000000005151510;
+    
+    q++;
+
+    q->dw[0] = GS_PRIM_TRIANGLE;
+    q->dw[1] = 0;
     q++;
 
     for(int i = 0; i < 3; i++){
-        q->dw[0] = (red & 0xff) | (green & 0xff) << 32;
-        q->dw[1] = (blue & 0xff) | (0x80 << 32);
+        q->dw[0] = (red & 0xff) | SHIFT_AS_I64((green & 0xff), 32);
+        q->dw[1] = (blue & 0xff) | SHIFT_AS_I64(0x80, 32);
         q++;
 
-        q->dw[0] = (tri[i + 0] << 4) | (tri[i + 1] << 4);
-        q->dw[1] = (tri[i + 2] << 4);
+        // 0xa -> 0xa0
+        // fixed point format - xxxx xxxx xxxx . yyyy
+        const int base = i * 3;
+        q->dw[0] = (tri[base + 0] << 4) | SHIFT_AS_I64((tri[base + 1] << 4), 32);
+        q->dw[1] = (tri[base + 2] << 4);
         q++;
     }
 
@@ -93,7 +123,7 @@ int main(){
     dma_channel_fast_waits(DMA_CHANNEL_GIF);
     
     // Initialize graphics mode
-    gs_init(640, 480, GS_PSM_32, GS_PSMZ_32, GRAPH_MODE_NTSC, GRAPH_MODE_FIELD);
+    gs_init(640, 480, GS_PSM_32, GS_PSMZ_24, GRAPH_MODE_NTSC, GRAPH_MODE_INTERLACED);
 
     // Clear the screen
     draw();
