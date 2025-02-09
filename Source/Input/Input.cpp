c8f79458e91a12c4210b4f3db6fe9a9fdc998ddd
ShikenNuggets <carter.rennick@gmail.com>
2025-02-09 15:46:44 -0500

    Global button checking

--- Source/Input/Input.cpp
+++ Source/Input/Input.cpp
@@ -34,12 +34,101 @@ void Input::Update(){
 	for(auto& port : ports){
 		port.Update();
 	}
+}
 
-	if(ports[0].ButtonDown(PAD_TRIANGLE, -1)){
-		LOG_VERBOSE("Triangle down!");
+static bool GetPortAndSlotForPlayerIdx(int& outPort, int& outSlot, int playerIdx){
+	BASIC_ASSERT(playerIdx >= 0 && playerIdx <= 8);
+	if(playerIdx < 0 || playerIdx > 8){
+		return false;
 	}
 
-	if(ports[0].ButtonUp(PAD_TRIANGLE, -1)){
-		LOG_VERBOSE("Triangle up!");
+	int playersAssociated = 0;
+	for(const auto& port : ports){
+		int slots = port.GetNumSlots();
+		if(playerIdx > playersAssociated + slots){
+			playersAssociated += slots;
+			continue;
+		}
+
+		outPort = port.GetPort();
+		outSlot = playerIdx - playersAssociated;
+		return true;
 	}
+
+	return false;
+}
+
+bool Input::ButtonDown(uint32_t button, int player){
+	if(player < 0){
+		for(const auto& port : ports){
+			if(port.ButtonDown(button)){
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	BASIC_ASSERT(player <= 8);
+	if(player > 8){
+		return false;
+	}
+
+	int port = 0;
+	int slot = 0;
+	if(!GetPortAndSlotForPlayerIdx(port, slot, player)){
+		return false;
+	}
+
+	return ports[port].ButtonDown(button, slot);
+}
+
+bool Input::ButtonUp(uint32_t button, int player){
+	if(player < 0){
+		for(const auto& port : ports){
+			if(port.ButtonUp(button)){
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	BASIC_ASSERT(player <= 8);
+	if(player > 8){
+		return false;
+	}
+
+	int port = 0;
+	int slot = 0;
+	if(!GetPortAndSlotForPlayerIdx(port, slot, player)){
+		return false;
+	}
+
+	return ports[port].ButtonUp(button, slot);
+}
+
+bool Input::ButtonHeld(uint32_t button, int player){
+	if(player < 0){
+		for(const auto& port : ports){
+			if(port.ButtonHeld(button)){
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	BASIC_ASSERT(player <= 8);
+	if(player > 8){
+		return false;
+	}
+
+	int port = 0;
+	int slot = 0;
+	if(!GetPortAndSlotForPlayerIdx(port, slot, player)){
+		return false;
+	}
+
+	return ports[port].ButtonHeld(button, slot);
 }
--- Source/Input/Input.hpp
+++ Source/Input/Input.hpp
@@ -1,9 +1,15 @@
 #ifndef PS2E_INPUT_HPP
 #define PS2E_INPUT_HPP
 
+#include <cstdint>
+
 namespace Input{
 	void Init();
 	void Update();
+
+	bool ButtonDown(uint32_t button, int player = -1);
+	bool ButtonUp(uint32_t button, int player = -1);
+	bool ButtonHeld(uint32_t button, int player = -1);
 };
 
 #endif //!PS2E_INPUT_HPP
