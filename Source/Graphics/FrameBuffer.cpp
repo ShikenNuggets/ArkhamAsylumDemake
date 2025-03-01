224b93a7be7ab185e5527bb1e61c0cb48023ef31
ShikenNuggets <carter.rennick@gmail.com>
2025-03-01 14:40:12 -0500

    Minor bug fix

--- Source/Graphics/FrameBuffer.cpp
+++ Source/Graphics/FrameBuffer.cpp
@@ -6,8 +6,8 @@
 
 FrameBuffer::FrameBuffer(unsigned int width, unsigned int height){
 	fb.address = graph_vram_allocate(width, height, GS_PSM_32, GRAPH_ALIGN_PAGE);
-	fb.width = 640;
-	fb.height = 480;
+	fb.width = width;
+	fb.height = height;
 	fb.mask = 0;
 	fb.psm = GS_PSM_32;
 
