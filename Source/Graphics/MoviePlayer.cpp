b2c253f35d1837a6ba193f83c3d6f9279c874a60
ShikenNuggets <carter.rennick@gmail.com>
2025-03-23 21:27:31 -0400

    Use one big (still uncompressed) movie file instead of many individual image frames

--- Source/Graphics/MoviePlayer.cpp
+++ Source/Graphics/MoviePlayer.cpp
@@ -18,41 +18,34 @@ void MoviePlayer::Update(){
 	if(currentFrameIdx >= numFrames){
 		return;
 	}
-
-	currentFrameIdx++; // TODO - Ideally don't skip frames, but this gives us adequate performance for now
+	
 	if(dupeThisFrame){
 		dupeThisFrame = false;
 		return;
 	}
 
+	currentFrameIdx++;
 	BindCurrentFrame();
 	dupeThisFrame = true;
 }
 
 void MoviePlayer::BindCurrentFrame(){
-	std::string frameName = std::string("host:") + path + "/" + path + "-";
-	if(currentFrameIdx < 10){
-		frameName += "0000" + std::to_string(currentFrameIdx);
-	}else if(currentFrameIdx < 100){
-		frameName += "000" + std::to_string(currentFrameIdx);
-	}else if(currentFrameIdx < 1'000){
-		frameName += "00" + std::to_string(currentFrameIdx);
-	}else if(currentFrameIdx < 10'000){
-		frameName += "0" + std::to_string(currentFrameIdx);
-	}else{
-		frameName += std::to_string(currentFrameIdx);
-	}
-
-	frameName += ".bmp.raw";
+	std::string frameName = path + std::string(".raw");
 
 	std::ifstream input(frameName, std::ios::binary);
 	if(!input.is_open()){
-		LOG_ERROR("Could not open %s for reading!", frameName.c_str());
+		LOG_ERROR("Could not open %s for reading!", frameName);
 		return;
 	}
 
+	constexpr int frameSize = 196'608;
+
 	buffer.clear();
-	buffer.insert(buffer.begin(), std::istreambuf_iterator<char>(input), {});
+	buffer.resize(frameSize);
+	//buffer.insert(buffer.begin(), std::istreambuf_iterator<char>(input), {});
+	
+	input.seekg((currentFrameIdx - 1) * frameSize);
+	input.read(reinterpret_cast<char*>(buffer.data()), frameSize);
 
 	if(currentFrame == nullptr){
 		currentFrame = new TextureBuffer(256, 256, buffer.data());
